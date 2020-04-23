ca69af0b0d870ec3e011f0039505940da0ec9c01
Nijarudeen  Rajak <50949038+black-nijar@users.noreply.github.com>
2020-04-23 15:29:27 +0530

    Update DBCONFIG.js

--- src/Components/DBCONFIG.js
+++ src/Components/DBCONFIG.js
@@ -3,13 +3,13 @@ import 'firebase/storage'
 // Your web app's Firebase configuration
  const firebaseConfig = {
   apiKey: "",
-  authDomain: "book-55e06.firebaseapp.com",
-  databaseURL: "https://book-55e06.firebaseio.com",
-  projectId: "book-55e06",
-  storageBucket: "book-55e06.appspot.com",
-  messagingSenderId: "670300519833",
-  appId: "1:670300519833:web:c3cb7070716b4bc376a708",
-  measurementId: "G-0XZYHB40QP"
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 const Firebase = firebase.initializeApp(firebaseConfig);
 const DB_CONFIG = Firebase.database().ref('data')
