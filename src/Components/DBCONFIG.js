c33607bdbb4a9277179664c7c0d12b214c908146
Nijar <nijarr2020@gmail.com>
2021-06-06 18:52:26 +0530

    firebase deploy

--- src/Components/DBCONFIG.js
+++ src/Components/DBCONFIG.js
@@ -1,10 +1,9 @@
 import firebase from "firebase";
 import "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {};
-
+var firebaseConfig = {};
+// Initialize Firebase
 const Firebase = firebase.initializeApp(firebaseConfig);
 const DB_CONFIG = Firebase.database().ref("data");
 export const storage = Firebase.storage();
-export const auth = Firebase.auth;
 export default DB_CONFIG;
